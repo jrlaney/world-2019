9f4a7b774ff17a8cc0aa9aadeca2d0747b9dd4f6
Walter Cerna <waltcerna@gmail.com>
2018-11-28 17:07:38 -0500

    removed agenda builder code to trim size, updated jplist

--- scripts/agenda-builder.js
+++ scripts/agenda-builder.js
@@ -1,19 +1,15 @@
 <script type="text/javascript">
 var allSessions = [];
 
-function getQueryStringParamValue(key)
-{
+function getQueryStringParamValue(key) {
 	var url = document.location.href;
 	var value = "";
 
-	if (url.indexOf('?') !== -1)
-	{
+	if (url.indexOf('?') !== -1) {
 		var queryStrings = url.substr(url.indexOf('?') + 1);
 
-		if (queryStrings.toLowerCase().indexOf(key.toLowerCase() + "=") !== -1)
-		{
-			var queryString = queryStrings.split("&").find(function (element)
-			{
+		if (queryStrings.toLowerCase().indexOf(key.toLowerCase() + "=") !== -1) {
+			var queryString = queryStrings.split("&").find(function (element) {
 				return element.toLowerCase().split("=")[0] === key.toLowerCase();
 			});
 			if (typeof queryString !== "undefined")
@@ -23,29 +19,23 @@ function getQueryStringParamValue(key)
 	return value;
 }
 
-function classifyText(text)
-{
+function classifyText(text) {
 	var returnValue = "";
-	if ($.isArray(text))
-	{
-		$.each($.unique(text), function (key, value)
-		{
+	if ($.isArray(text)) {
+		$.each($.unique(text), function (key, value) {
 			returnValue += " " + value.toLowerCase().replace(" ", "").replace(/[^a-z]+/g, '');
 		});
-	}
-	else
-	{
+	} else {
 		returnValue = text.toLowerCase().replace(" ", "").replace(/[^a-z]+/g, '');
 	}
 	return returnValue;
 }
 
-function tagCheckboxDropdownFilter($filter, $tag, dataPathId)
-{
+function tagFilterListItems($filter, $tag, dataPathId) {
 	var dataPath = dataPathId + classifyText($tag.text());
 	// update value and trigger change in field
 	$filter.find('#' + dataPath).trigger('click');
-	$filter.find('.jplist-dd-panel').addClass('changed');
+	$filter.find('jplist-checkbox-path-filter').addClass('changed');
 
 	// move window to focus on field changed
 	$('html, body').animate({
@@ -54,34 +44,27 @@ function tagCheckboxDropdownFilter($filter, $tag, dataPathId)
 	// select first pagination page
 	$('.pagination').find('button[data-number="0"]').trigger('click');
 	// remove changed style after a second
-	setTimeout(function ()
-	{
-		$filter.find('.jplist-dd-panel').removeClass('changed');
+	setTimeout(function () {
+		$filter.find('jplist-checkbox-path-filter').removeClass('changed');
 	}, 1000);
 }
 
-function tagFilterInit()
-{
-	$('.program-tag').click(function ()
-	{
-		tagCheckboxDropdownFilter($('#program-filter'), $(this), '');
+function tagFilterInit() {
+	$('.program-tag').click(function () {
+		tagFilterListItems($('#program-filter'), $(this), '');
 	});
-	$('.role-tag').click(function ()
-	{
-		tagCheckboxDropdownFilter($('#role-filter'), $(this), '');
+	$('.role-tag').click(function () {
+		tagFilterListItems($('#role-filter'), $(this), '');
 	});
-	$('.topic-tag').click(function ()
-	{
-		tagCheckboxDropdownFilter($('#topic-filter'), $(this), '');
+	$('.topic-tag').click(function () {
+		tagFilterListItems($('#topic-filter'), $(this), '');
 	});
 }
 
-function GetSessionsFromCookie()
-{
+function GetSessionsFromCookie() {
 	var myAgenda = $.cookie("myAgenda");
 	var myArticles = [];
-	if (myAgenda !== undefined)
-	{
+	if (myAgenda !== undefined) {
 		myArticles = JSON.parse(myAgenda);
 		if (typeof myArticles === "string")
 			myArticles = JSON.parse(myArticles);
@@ -89,43 +72,34 @@ function GetSessionsFromCookie()
 	return myArticles;
 }
 
-function populateMyAgendaSessions()
-{
+function populateMyAgendaSessions() {
 	var myArticles = GetSessionsFromCookie();
 	$("#my-agenda-list").html("");
 
 	//Looping through each data point from airtable and creating the cards
-	$.each(allSessions, function (key, value)
-	{
-		if (value.Title != null && value.Publish && myArticles.indexOf(value.Id) > -1)
-		{
+	$.each(allSessions, function (key, value) {
+		if (value.Title != null && value.Publish && myArticles.indexOf(value.Id) > -1) {
 			var tags = "";
-			if (value.Program != null)
-			{
+			if (value.Program != null) {
 				$.each(value.Program,
-					function (k, v)
-					{
+					function (k, v) {
 						tags += "<span class=\"text-label program-tag " + classifyText(v) + "\">" + v + "</span>";
 					});
 			}
 
-			if (value.RolePersona != null)
-			{
+			if (value.RolePersona != null) {
 				$.each(value.RolePersona,
-					function (k, v)
-					{
+					function (k, v) {
 						tags += "<span class=\"text-label role-tag " + classifyText(v) + "\">" + v + "</span>";
 					});
 			}
-			if (value.Topic != null)
-			{
+			if (value.Topic != null) {
 				$.each(value.Topic,
-					function (k, v)
-					{
+					function (k, v) {
 						tags += "<span class=\"text-label topic-tag " + classifyText(v) + "\">" + v + "</span>";
 					});
 			}
-			var description = ((value.MarCommReviewAbstract != null) ? ((value.MarCommReviewAbstract.length > 100) ? (value.MarCommReviewAbstract.substring(0, 1000) + "...") : value.MarCommReviewAbstract) : "");
+			var description = ((value.MarCommReviewAbstract != null) ? ((value.MarCommReviewAbstract.length > 1000) ? (value.MarCommReviewAbstract.substring(0, 1000) + "...") : value.MarCommReviewAbstract) : "");
 			description = description.replace(/\n/g, '<br />');
 			$("#my-agenda-list").append("" +
 				"<article class=\"grid-item agenda-list\" id=\"myAgenda-" + value.Id + "\">" +
@@ -143,111 +117,72 @@ function populateMyAgendaSessions()
 		}
 	});
 }
-
-function updateAddtoMyAgendaButtons()
-{
-	var myArticles = GetSessionsFromCookie();
-
-	$.each($("#agendaCards article"), function (key, article)
-	{
-		var id = $(article).attr('id');
-		var index = myArticles.indexOf(id);
-		if (index === -1)
-			$(article).children(".add-to-agenda").removeClass("added");
-		else
-			$(article).children(".add-to-agenda").addClass("added");
-	});
-}
-
-function copyURL()
-{
-	var copyText = document.getElementById("inputURL");
-	copyText.select();
-	document.execCommand("copy");
-}
 //end first snippet
 
-
-
-
-
-
-
-
-
 //start second snippet
-
-$('document').ready(function ()
-{
+$('document').ready(function () {
 	var sessions = getQueryStringParamValue("sessions");
-	if (sessions.length > 0)
-	{
+	if (sessions.length > 0) {
 		var myArticles = sessions.replace("[", "").replace("]", "").split("%22").join("").split(",");
-		$.cookie("myAgenda", JSON.stringify(myArticles), { path: '/', expires: 7 });
+		$.cookie("myAgenda", JSON.stringify(myArticles), {
+			path: '/',
+			expires: 7
+		});
 	}
 	//Grabbing API Information
-	$.get("https://www.microstrategy.com/api/GetAirTableData", function (data)
-	{
+	$.get("https://www.microstrategy.com/api/GetAirTableData", function (data) {
 		allSessions = data;
 
 		//Creating empty array variables for each of the filtering options
 		var program = [];
 		var role = [];
 		var topic = [];
-		$.each(allSessions, function (key, value)
-		{
-			if (value.Title != null && value.Publish)
-			{
+		$.each(allSessions, function (key, value) {
+			if (value.Title != null && value.Publish) {
 				var tags = "";
-				if (value.Program != null)
-				{
+				if (value.Program != null) {
 					$.each(value.Program,
-						function (k, v)
-						{
+						function (k, v) {
 							tags += "<span class=\"text-label program-tag " + classifyText(v) + "\">" + v + "</span>";
 							if (program.indexOf(v) === -1)
 								program.push(v);
 						});
 				}
-				if (value.RolePersona != null)
-				{
+				if (value.RolePersona != null) {
 					$.each(value.RolePersona,
-						function (k, v)
-						{
+						function (k, v) {
 							tags += "<span class=\"text-label role-tag " + classifyText(v) + "\">" + v + "</span>";
 							if (role.indexOf(v) === -1)
 								role.push(v);
 						});
 				}
-				if (value.Topic != null)
-				{
+				if (value.Topic != null) {
 					$.each(value.Topic,
-						function (k, v)
-						{
+						function (k, v) {
 							tags += "<span class=\"text-label topic-tag " + classifyText(v) + "\">" + v + "</span>";
 							if (topic.indexOf(v) === -1)
 								topic.push(v);
 						});
 				}
 
-				var description = ((value.MarCommReviewAbstract != null) ? ((value.MarCommReviewAbstract.length > 100) ? (value.MarCommReviewAbstract.substring(0, 1000) + "...") : value.MarCommReviewAbstract) : "");
+				var description = ((value.MarCommReviewAbstract != null) ? ((value.MarCommReviewAbstract.length > 1000) ? (value.MarCommReviewAbstract.substring(0, 1000) + "...") : value.MarCommReviewAbstract) : "");
 				description = description.replace(/\n/g, '<br />');
 				$("#agendaCards").append("" +
 					"<article class=\"grid-item agenda-list\" id=\"" + value.Id + "\">" +
 					"<h3 class=\"session-title\">" + value.Title + "</h3>" +
 					"<h4 class=\"session-speaker hide\">" + ((value.Speaker != null) ? value.Speaker : "") + "</h4>" +
 					"<p class=\"session-type hide\">" + value.SessionType + "</p>" +
+					"<p class=\"session-start-time hide\">" + value.SessionStartDateTime + "</p>" +
 					"<div class=\"session-details hide\">" +
 					"<p class=\"session-description\">" + description + "</p>" +
 					"</div>" +
 					"<div class=\"text-label-group tags\">" +
 					"<span class=\"tag-heading\">Tags:</span>" + tags + "</div>" +
-					"<div class='add-to-agenda'><span class=\"plus-icon\"></span><span class=\"txt-add\">Add to</span><span class=\"txt-remove\">Remove from</span> my agenda</div>" +
+					//"<div class='add-to-agenda'><span class=\"plus-icon\"></span><span class=\"txt-add\">Add to</span><span class=\"txt-remove\">Remove from</span> my agenda</div>" +
 					"<span class=\"details-expand\">" + 'See Details' + "</span>" +
 					"</article>");
 			}
 		});
-		updateAddtoMyAgendaButtons();
 
 		//Sorting items
 		program.sort();
@@ -255,18 +190,15 @@ $('document').ready(function ()
 		topic.sort();
 		//Adding text dynamically from airtable to each of filter slots
 		var p = $('#program-filter > ul');
-		$.each(program, function (key, value)
-		{
+		$.each(program, function (key, value) {
 			p.append($("<li></li>").append($("<input>").attr("id", classifyText(value)).attr("data-path", "." + classifyText(value)).attr("type", "checkbox")).append($("<label></label>").attr("for", classifyText(value)).text(value)));
 		});
 		var r = $('#role-filter > ul');
-		$.each(role, function (key, value)
-		{
+		$.each(role, function (key, value) {
 			r.append($("<li></li>").append($("<input>").attr("id", classifyText(value)).attr("data-path", "." + classifyText(value)).attr("type", "checkbox")).append($("<label></label>").attr("for", classifyText(value)).text(value)));
 		});
 		var t = $('#topic-filter > ul');
-		$.each(topic, function (key, value)
-		{
+		$.each(topic, function (key, value) {
 			t.append($("<li></li>").append($("<input>").attr("id", classifyText(value)).attr("data-path", "." + classifyText(value)).attr("type", "checkbox")).append($("<label></label>").attr("for", classifyText(value)).text(value)));
 		});
 
@@ -275,174 +207,55 @@ $('document').ready(function ()
 			itemPath: '.grid-item',
 			panelPath: '.jp-lists',
 			effect: 'fade',
-			redrawCallback: function ()
-			{
+			redrawCallback: function () {
 				tagFilterInit();
 			}
 		});
 	}); //end of api pull
 
-	$('#filter-btn').click(function (event)
-	{
-		event.preventDefault();
-		$('#filter-box').slideToggle('slow');
-	});
+	$(function () {
 
-	$('#list-view-btn').click(function (e)
-	{
-		e.preventDefault();
-		$.each($('.session-browse .grid-item'), function (key, value)
-		{
-			$(value).removeClass('agenda-card col lg-4 xl-3').addClass('agenda-list');
+		var $sidebar = $("#filter-box"),
+			$window = $(window),
+			offset = $sidebar.offset(),
+			topPadding = 0;
+
+		$window.scroll(function () {
+			if ($window.scrollTop() > offset.top) {
+				$sidebar.stop().animate({
+					marginTop: $window.scrollTop() - offset.top
+				});
+			} else {
+				$sidebar.stop().animate({
+					marginTop: 0
+				});
+			}
 		});
-		$(this).toggleClass('current');
-		$('#grid-view-btn').removeClass('current');
 	});
+// End of second snippet
 
-	$(document).on('click', '.add-to-agenda', function ()
-	{
-		var classes = $(this).attr("class");
-		var article = $(this).parent();
-		var id = article.attr('id');
-		var myAgendaArticle = id.indexOf("myAgenda-") > -1;
-		id = id.replace("myAgenda-", "");
-		var myAgenda = $.cookie("myAgenda");
-		var myArticles = GetSessionsFromCookie();
-		// End of second snippet
-
-
-
-
-
-
-		// Third snippet starts here
-		if (myAgenda !== undefined)
-		{
-			var index = myArticles.indexOf(id);
-			if (classes.indexOf("added") === -1)
-			{
-				if (index === -1)
-					myArticles.push(id);
-				$(this).addClass("added");
-			}
-			else
-			{
-				if (index > -1)
-					myArticles.splice(index, 1);
-				$(this).removeClass("added");
-			}
-		}
-		else
-		{
-			if (classes.indexOf("added") === -1)
-			{
-				myArticles.push(id);
-				$(this).addClass("added");
-			}
-			else
-				$(this).removeClass("added");
-		}
-		$.cookie("myAgenda", JSON.stringify(myArticles), { path: '/', expires: 7 });
-
-		if (myAgendaArticle)
-			article.fadeOut(1000, function () { $(this).remove(); });
-	});
-
-
-	$('#grid-view-btn').click(function (e)
-	{
-		e.preventDefault();
-		$.each($('.session-browse .agenda-list'), function (key, value)
-		{
-			$(value).removeClass('agenda-list').addClass('agenda-card col lg-4 xl-3');
-		});
-		$(this).toggleClass('current');
-		$('#list-view-btn').removeClass('current');
-	});
-
-	$(document).on('click', '.details-expand', function ()
-	{
+// Third snippet starts here
+	$(document).on('click', '.details-expand', function () {
 		var details = $('.session-details');
 		$(this).parent('.grid-item').find(details).slideToggle('fast');
 		$(this).parent('.grid-item').find(details).toggleClass('hide');
 		$(this).text($(this).text() === 'See Details' ? 'Hide Details' : 'See Details');
 	});
 
-	$('.tabs-menu .tab-toggle').click(function (event)
-	{
+	$('.tabs-menu .tab-toggle').click(function (event) {
 		event.preventDefault();
 		$(this).addClass('current');
 		$(this).siblings().removeClass('current');
 
 		var tab = $(this).attr('href');
 		$('.tab-content').not(tab).addClass('hide-tab');
-		setTimeout(function ()
-		{
+		setTimeout(function () {
 			$('.tab-content').not(tab).css('display', 'none');
 		}, 200);
 		$(tab).removeClass('hide-tab');
-		setTimeout(function ()
-		{
+		setTimeout(function () {
 			$(tab).css('display', 'block');
 		}, 400);
-		if (tab.indexOf("#my-agenda") > -1)
-		{
-			populateMyAgendaSessions();
-			$("#shareURL").hide();
-		}
-		else if (tab.indexOf("#sessions") > -1)
-			updateAddtoMyAgendaButtons();
-	});//tab toogle script
-
-	$("#print-agenda").click(function ()
-	{
-		var html = "<html>";
-		html += "<head>";
-		html += "<link rel='Stylesheet' type='text/css' href='http://www.microstrategy.com/Strategy/media/external-assets/events-support/world-2019/world2019-print-agenda-sheet.css' />";
-		html += "</head>";
-		html += "<body>";
-		html += "<div class='logo-zone'><img class='logo-img' src='https://www.microstrategy.com/Strategy/media/external-assets/events-support/world-2019/world2019-logo.svg'><h2>MicroStrategy World 2019<span>February 4-6 2019</span><span>Phoenix, Arizona</span></h2><h3>http://events.microstrategy.com/world2019</h3></div>";
-		html += "<h1 class='agenda-headline'>My Agenda</h1><div class='agenda-body'><h5 class='text-uppercase text-bold program-label'>Theme</h5><h5 class='text-uppercase text-bold topic-label'>Topic</h5><h5 class='text-uppercase text-bold role-label'>Role</h5>";
-		html += $("#my-agenda-list").html();
-		html += "</div>";
-		html += "</body>";
-		html += "</html>";
-
-		var printWin = window.open('', '', 'left=0,top=0,toolbar=0,scrollbars=0,status=0');
-		printWin.document.write(html);
-		printWin.document.close();
-		printWin.focus();
-		setTimeout(function ()
-		{
-			printWin.print();
-		}, 500);
-		setTimeout(function ()
-		{
-			printWin.close();
-		}, 500);
-	});
-
-	$("#share-agenda").click(function ()
-	{
-		var shareURL = window.location.href;
-		var cookieValue = $.cookie("myAgenda").replace("[", "").replace("]", "").split("%22").join("").split("\"").join("");
-		if (shareURL.indexOf("?") > -1)
-			shareURL = shareURL + "&sessions=" + cookieValue;
-		else
-			shareURL = shareURL + "?sessions=" + cookieValue;
-
-		$("#inputURL").val(shareURL);
-		$("#shareURL").show();
-	});
-
-	$("#buttonCopy").click(function ()
-	{
-		$(this).text("Copied!");
-
-		setTimeout(function ()
-		{
-			$("#buttonCopy").text("Copy");
-		}, 3000);
-	});
+	}); //tab toogle script
 });
 </script>
