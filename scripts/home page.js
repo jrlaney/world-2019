009bb11c2acc77c04b9329f21b52a40db690975f
Walter Cerna <waltcerna@gmail.com>
2018-12-05 14:43:30 -0500

    renamed file to home-page but didnt remove this file

--- scripts/home page.js
+++ scripts/home page.js
@@ -1,8 +1,7 @@
-<script>
+
 	//add a page specific class to the body for targeting
 	document.getElementsByTagName('body')[0].className+='home-page';
 
 	jQuery(document).ready(function ($) {
 		$('.top .navigation .logo-lockup').append('<img class="mstr-logo" src="https://www.microstrategy.com/Strategy/media/external-assets/events-support/microstrategy-logo_red.svg" />');
 	});
-</script>
